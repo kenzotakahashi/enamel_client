a77cadb99f8377a87e3fab8b316caa7ee60059b1
kenzotakahashi <kenzotakahashi2@gmail.com>
2019-01-03 17:51:09 +0900

    add emoji

--- src/components/CommentBox.vue
+++ src/components/CommentBox.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="comment-box">
     <div class="text-form">
-      <textarea class="no-outline" type="text" name="comment" v-model="form.body"
-        placeholder="Add comment"
+      <textarea class="no-outline" ref="textarea" type="text" name="comment"
+        v-model="form.body" placeholder="Add comment"
+        @focus="emoji=false"
         @keydown.shift.enter="createComment"
         @keydown.ctrl.enter="createComment"
         @keydown.meta.enter="createComment"></textarea>
+    </div>
+    <div class="emoji-button" >
+      <Picker v-show="emoji" set='emojione' :sheetSize='32' :native='true' @select="addEmoji" />
+      <i class="far fa-smile" @click.stop="toggleEmoji"></i>
     </div>
     <div v-show="form.body" class="button">
       <el-button type="primary" size="small" @click="createComment">Send</el-button>
@@ -13,21 +18,44 @@
   </div>
 </template>
 
+
+
 <script>
 import { GetComments, CreateComment } from '../constants/query.gql'
+import { Picker } from 'emoji-mart-vue'
 
 export default {
   props: ['parent'],
+  components: {
+    Picker
+  },
   data() {
     return {
+      emoji: false,
       form: {
         body: ''
       }
     }
   },
   methods: {
+    toggleEmoji() {
+      this.emoji = !this.emoji
+    },
+    addEmoji(emoji) {
+      const textarea = this.$refs.textarea
+      const cursorPosition = textarea.selectionEnd
+      const start = this.form.body.substring(0, textarea.selectionStart)
+      const end = this.form.body.substring(textarea.selectionStart)
+      this.form.body = start + emoji.native + end
+      this.emoji = false
+      textarea.focus()
+      this.$nextTick(() => {
+        textarea.selectionEnd = cursorPosition + emoji.native.length
+      })
+    },
     createComment() {
       if (!this.form.body) return
+      this.emoji = false
       const target = this.parent
       this.$apollo.mutate({
         mutation: CreateComment,
@@ -84,6 +112,21 @@ export default {
     width: 100%;
   }
 }
+
+.emoji-button {
+  position: relative;
+  cursor: pointer;
+  padding: 4px;
+  .fa-smile {
+    font-size: 18px;
+  }  
+  .emoji-mart {
+    position: absolute;
+    bottom: 55px;
+    right: 0;
+  }
+}
+
 </style>
 
 
